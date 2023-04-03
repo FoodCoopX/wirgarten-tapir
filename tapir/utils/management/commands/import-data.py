8205f07f07f8b9137682badec85ec7ee33c3fab4
dev@kirchner-bln.de <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>
2023-04-03 18:30:09 +0200

    add complete user data reset; fix missing field alignments

--- tapir/utils/management/commands/import-data.py
+++ tapir/utils/management/commands/import-data.py
@@ -1,22 +1,33 @@
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
-from django.core.management.base import BaseCommand, CommandError
-import django.db
-from tapir.wirgarten.models import Member, Subscription, CoopShareTransaction, TapirUser, GrowingPeriod, \
-    MandateReference, Product, PickupLocation
-from tapir.wirgarten.service.member import get_or_create_mandate_ref
 import csv
+
+import django.db
+
+from tapir.wirgarten.models import Member, Subscription, CoopShareTransaction, TapirUser, GrowingPeriod, \
+    Product, PickupLocation
+from tapir.wirgarten.service.member import get_or_create_mandate_ref
+
 
 class Command(BaseCommand):
     help = 'Imports data from CSV files into the database'
 
     def add_arguments(self, parser):
-        parser.add_argument('--type', nargs=1, choices=['members', 'shares', 'subscriptions'], required=True)
-        parser.add_argument('--file', nargs=1, required=True)
+        parser.add_argument('--type', nargs=1, choices=['members', 'shares', 'subscriptions'])
+        parser.add_argument('--file', nargs=1)
         parser.add_argument('--delete-all', action='store_true')
+        parser.add_argument('--reset-all', action='store_true')
 
     def handle(self, *args, **options):
-        #print(args)
-        print(options)
+        # print(options)
+
+        if options["reset_all"]:
+            Member.objects.all().delete()
+            Subscription.objects.all().delete()
+            CoopShareTransaction.objects.all().delete()
+            return
+
+        # check if type and file params are present
+        if not options["file"][0] or not options["type"][0]:
+            print("If not --reset-all is used, parameters --type and --file must be present.")
         filepath = options["file"][0]
         type = options["type"][0]
         delete_all = options["delete_all"]
@@ -63,13 +74,13 @@ class Command(BaseCommand):
                     CoopShareTransaction.objects.all().delete()
                 for row in reader:
                     print(row)
-                    #{'Mitgliedsnummer': '1', 'Bewegungsart (Z,Ü,K)': 'Z', 'Datum': '2017-03-10', 'Anzahl Anteile': '2', 'Wert Anteile': '100', 'Übertragungspartner': '', 'Wirkung Kündigung': ''}
+                    # {'Mitgliedsnummer': '1', 'Bewegungsart (Z,Ü,K)': 'Z', 'Datum': '2017-03-10', 'Anzahl Anteile': '2', 'Wert Anteile': '100', 'Übertragungspartner': '', 'Wirkung Kündigung': ''}
                     qu = int(row["Anzahl Anteile"])
                     transfer_member = None
                     valid_date = row["Datum"]
                     if row["Übertragungspartner"] != '':
                         try:
-                            transfer_member=Member.objects.get(id=row["Übertragungspartner"])
+                            transfer_member = Member.objects.get(id=row["Übertragungspartner"])
                             print(transfer_member)
                         except ObjectDoesNotExist as e:
                             print("Transfer Member not found!")
@@ -99,9 +110,9 @@ class Command(BaseCommand):
                             transfer_member=transfer_member
                         )
                     except django.db.Error as e:
-                        print("Database Error occured",e.__cause__)
+                        print("Database Error occured", e.__cause__)
                     except ValidationError as e:
-                        print("Validation Error occured",e.messages)
+                        print("Validation Error occured", e.messages)
             if type == "subscriptions":
                 if delete_all:
                     Subscription.objects.all().delete()
@@ -118,18 +129,18 @@ class Command(BaseCommand):
                             if row["E-Mail-Adresse"] != "":
                                 m = TapirUser.objects.get(email=row["E-Mail-Adresse"])
                             else:
-                                print("No data to identify TapirUser in Vertrag ",row["VertragNr"])
+                                print("No data to identify TapirUser in Vertrag ", row["VertragNr"])
                     except django.core.exceptions.ObjectDoesNotExist as e:
                         print(row)
-                        print("Database Error: TapirUser not found",e.__cause__)
+                        print("Database Error: TapirUser not found", e.__cause__)
                         continue
                     except django.db.Error as e:
                         print(row)
-                        print("Database Error occured with MemberNo",e.__cause__)
+                        print("Database Error occured with MemberNo", e.__cause__)
                         continue
                     except ValidationError as e:
                         print(row)
-                        print("Validation Error occured",e.messages)
+                        print("Validation Error occured", e.messages)
                         continue
                     # identify MandateRef
                     mref = get_or_create_mandate_ref(m)
@@ -142,7 +153,7 @@ class Command(BaseCommand):
                             continue
                     except django.core.exceptions.ObjectDoesNotExist as e:
                         print(row)
-                        print("Product not found",e.__cause__)
+                        print("Product not found", e.__cause__)
                         continue
                     # prepare cancellation value
                     if row["cancellation.ts"] != "":
@@ -154,29 +165,24 @@ class Command(BaseCommand):
                             member_id=m.id,
                             quantity=float(row["Quantity"]),
                             start_date=row["Vertragsbeginn"],
-                            end_date="2023-06-30",
-                            cancellation_ts=ts_cancel ,
+                            end_date=row["vertragsende"],
+                            cancellation_ts=ts_cancel,
                             solidarity_price=row["Solidarpreis in Prozent"],
                             mandate_ref_id=mref.ref,
                             period_id=period.id,
                             product_id=prod.id,
-                            consent_ts="2023-01-01 12:00:00",
+                            consent_ts=row["consent_vertragsgrundsätze"],
                         )
                         print("Subscription object successfully created.")
                     except django.db.Error as e:
                         print(row)
-                        print("Database Error occured with create subscription: ",e.__cause__)
+                        print("Database Error occured with create subscription: ", e.__cause__)
                         continue
                     except ValidationError as e:
                         print(row)
-                        print("Validation Error occured with create subscription: ",e.messages)
+                        print("Validation Error occured with create subscription: ", e.messages)
                         continue
                     except ValueError as e:
                         print(row)
-                        print("Value Error occured with create subscription: ",e)
+                        print("Value Error occured with create subscription: ", e)
                         continue
-
-
-
-
-
