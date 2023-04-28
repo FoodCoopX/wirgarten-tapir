4d6d7fad010d3a9bd7abc40fe0b9df65927662d3
Michael Kleimann <mail@mkleimann.com>
2023-04-28 11:21:10 +0200

    Fix settings

--- tapir/wirgarten/service/email.py
+++ tapir/wirgarten/service/email.py
@@ -40,7 +40,9 @@ def send_email(to_email: [str], subject: str, content: str, variables: dict = {}
         subject=subject,
         body=email_body,
         to=to_email,
-        bcc=[settings.EMAIL_AUTO_BCC] if hasattr(settings, "EMAIL_AUTO_BCC") else None,
+        bcc=[settings.EMAIL_AUTO_BCC]
+        if hasattr(settings, "EMAIL_AUTO_BCC") and settings.EMAIL_AUTO_BCC
+        else None,
         from_email=settings.EMAIL_HOST_SENDER,
         headers={
             "From": f"{get_parameter_value(Parameter.SITE_NAME)} <{settings.EMAIL_HOST_SENDER}>"
