72f5ed323185ad67b53c2f6e301540c4ae9ca328
Théophile Madet <theo.madet@posteo.net>
2024-09-25 11:15:50 +0200

    Added test test_transferCoopShares_default_newMembersEntryDateIsTransferDate

---
+++ tapir/wirgarten/tests/integration/shares/test_transfer_coop_shares.py
@@ -0,0 +1,33 @@
+from tapir.wirgarten.models import Member
+from tapir.wirgarten.parameters import (
+    ParameterDefinitions,
+)
+from tapir.wirgarten.service.member import transfer_coop_shares
+from tapir.wirgarten.tests.factories import MemberFactory, MemberWithCoopSharesFactory
+from tapir.wirgarten.tests.test_utils import TapirIntegrationTest
+from tapir.wirgarten.utils import get_today
+
+
+class TestTransferCoopShares(TapirIntegrationTest):
+    def setUp(self):
+        ParameterDefinitions().import_definitions()
+
+    def test_transferCoopShares_default_newMembersEntryDateIsTransferDate(self):
+        receiving_member: Member = MemberFactory.create()
+        giving_member: Member = MemberWithCoopSharesFactory.create(
+            add_coop_shares__quantity=4
+        )
+
+        self.assertEqual(0, receiving_member.coop_shares_quantity)
+        self.assertEqual(4, giving_member.coop_shares_quantity)
+
+        transfer_coop_shares(
+            origin_member_id=giving_member.id,
+            target_member_id=receiving_member.id,
+            quantity=4,
+            actor=None,
+        )
+
+        self.assertEqual(4, receiving_member.coop_shares_quantity)
+        self.assertEqual(0, giving_member.coop_shares_quantity)
+        self.assertEqual(get_today(), receiving_member.coop_entry_date)
