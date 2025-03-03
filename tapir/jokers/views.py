da632c8b46a98ee47b101842a6b7c7badb5aa91d
Théophile Madet <theo.madet@posteo.net>
2025-03-03 16:50:54 +0100

    Added a first API view and automatic API schema generation

--- tapir/jokers/views.py
+++ tapir/jokers/views.py
@@ -1,37 +1,23 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from tapir.jokers.serializers import JokerSerializer, GetJokersRequestSerializer
+from tapir.jokers.models import Joker
+from tapir.jokers.serializers import JokerSerializer
+from tapir.wirgarten.utils import check_permission_or_self
 
 
-class GetJokers(APIView):
+class GetMemberJokers(APIView):
     @extend_schema(
         responses={200: JokerSerializer(many=True)},
-        request=GetJokersRequestSerializer,
+        parameters=[OpenApiParameter(name="member_id", type=str)],
     )
-    def post(self, request):
-        request_serializer = SendAnswerGermanWordSerializer(data=request.data)
-        request_serializer.is_valid(raise_exception=True)
-
-        answer = request_serializer.validated_data["answer"]
-        word = get_object_or_404(Word, id=request_serializer.validated_data["word_id"])
-        GermanWordResult.objects.create(answer=answer, word=word, user=request.user)
-
-        learned = False
-        if WordLearnedChecker.is_word_learned(request.user, word):
-            learned = True
-            LearnedWord.objects.create(user=request.user, word=word)
+    def get(self, request):
+        member_id = request.query_params.get("member_id")
+        check_permission_or_self(member_id, request)
 
         return Response(
-            WasAnswerCorrectSerializer(
-                {
-                    "correct": answer == word.gender,
-                    "learned": learned,
-                    "nb_answers_correct_in_a_row": WordLearnedChecker.nb_correct_in_a_row(
-                        request.user, word
-                    ),
-                    "repetitions_to_learn": LearningConfig.REPETITIONS_TO_LEARN,
-                }
-            ).data,
+            JokerSerializer(Joker.objects.filter(member_id=member_id), many=True).data,
             status=status.HTTP_200_OK,
         )
