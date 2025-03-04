fafe8cea49c6cbfe3232d966cbe8d711218df442
Théophile Madet <theo.madet@posteo.net>
2025-03-04 16:27:54 +0100

    Replaced the old delivery list with the react one, added loading placeholders to the delivery list.

--- src_frontend/member_profile/DeliveryListCard.tsx
+++ src_frontend/member_profile/DeliveryListCard.tsx
@@ -7,7 +7,7 @@ import {
   PickupLocationOpeningTime,
 } from "../api-client";
 import { useApi } from "../hooks/useApi";
-import { Card, Spinner, Table } from "react-bootstrap";
+import { Card, Placeholder, Table } from "react-bootstrap";
 import TapirButton from "../components/TapirButton.tsx";
 import { formatDateText } from "../utils/formatDateText.ts";
 import RelativeTime from "dayjs/plugin/relativeTime";
@@ -48,6 +48,68 @@ const DeliveryListCard: React.FC<DeliveryListCardProps> = ({ memberId }) => {
       .finally(() => setDeliveriesLoading(false));
   }, []);
 
+  function loadingPlaceholder() {
+    return Array.from(Array(7).keys()).map((index) => {
+      return (
+        <tr key={index}>
+          {Array.from(Array(3).keys()).map((index) => {
+            return (
+              <td key={index}>
+                <Placeholder lg={10} />
+              </td>
+            );
+          })}
+        </tr>
+      );
+    });
+  }
+
+  function dateCell(delivery: Delivery) {
+    return (
+      <div className={"d-flex flex-column"}>
+        <strong>{formatDateText(delivery.deliveryDate)}</strong>
+        <small>{dayjs().to(dayjs(delivery.deliveryDate))}</small>
+      </div>
+    );
+  }
+
+  function productCell(delivery: Delivery) {
+    return (
+      <div className={"d-flex flex-column"}>
+        {delivery.subscriptions.map((subscription) => {
+          return (
+            <div key={subscription.id}>
+              {subscription.quantity}
+              {" × "}
+              {subscription.product.name} {subscription.product.type.name}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  function pickupLocationCell(delivery: Delivery) {
+    return (
+      <div className={"d-flex flex-column align-items-center"}>
+        <strong>{delivery.pickupLocation.name}</strong>
+        <div style={{ width: "32px" }}>
+          <TapirButton
+            variant={"outline-secondary"}
+            icon={"schedule"}
+            size={"sm"}
+            onClick={() => {
+              setSelectedPickupLocation(delivery.pickupLocation);
+              setSelectedPickupLocationOpeningTimes(
+                delivery.pickupLocationOpeningTimes,
+              );
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <Card>
@@ -64,77 +126,30 @@ const DeliveryListCard: React.FC<DeliveryListCardProps> = ({ memberId }) => {
           </div>
         </Card.Header>
         <Card.Body className={"p-0"}>
-          {deliveriesLoading ? (
-            <Spinner />
-          ) : (
-            <div style={{ overflowY: "scroll", maxHeight: "30em" }}>
-              <Table striped hover responsive className={"text-center"}>
-                <thead>
-                  <tr>
-                    <th>Datum</th>
-                    <th>Produkte</th>
-                    <th>Abholort</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {deliveries.map((delivery, index) => {
-                    return (
-                      <tr key={index}>
-                        <td>
-                          <div className={"d-flex flex-column"}>
-                            <strong>
-                              {formatDateText(delivery.deliveryDate)}
-                            </strong>
-                            <small>
-                              {dayjs().to(dayjs(delivery.deliveryDate))}
-                            </small>
-                          </div>
-                        </td>
-                        <td>
-                          <div className={"d-flex flex-column"}>
-                            {delivery.subscriptions.map(
-                              (subscription, index) => {
-                                return (
-                                  <div key={subscription.id}>
-                                    {subscription.quantity}
-                                    {" × "}
-                                    {subscription.product.name}{" "}
-                                    {subscription.product.type.name}
-                                  </div>
-                                );
-                              },
-                            )}
-                          </div>
-                        </td>
-                        <td>
-                          <div
-                            className={"d-flex flex-column align-items-center"}
-                          >
-                            <strong>{delivery.pickupLocation.name}</strong>
-                            <div style={{ width: "32px" }}>
-                              <TapirButton
-                                variant={"outline-secondary"}
-                                icon={"schedule"}
-                                size={"sm"}
-                                onClick={() => {
-                                  setSelectedPickupLocation(
-                                    delivery.pickupLocation,
-                                  );
-                                  setSelectedPickupLocationOpeningTimes(
-                                    delivery.pickupLocationOpeningTimes,
-                                  );
-                                }}
-                              />
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-            </div>
-          )}
+          <div style={{ overflowY: "scroll", maxHeight: "30em" }}>
+            <Table striped hover responsive className={"text-center"}>
+              <thead>
+                <tr>
+                  <th>Datum</th>
+                  <th>Produkte</th>
+                  <th>Abholort</th>
+                </tr>
+              </thead>
+              <tbody>
+                {deliveriesLoading
+                  ? loadingPlaceholder()
+                  : deliveries.map((delivery, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{dateCell(delivery)}</td>
+                          <td>{productCell(delivery)}</td>
+                          <td>{pickupLocationCell(delivery)}</td>
+                        </tr>
+                      );
+                    })}
+              </tbody>
+            </Table>
+          </div>
         </Card.Body>
       </Card>
       {selectedPickupLocation && (
