1e232ee547a8e9a188295de6d7f79438188d70ef
Théophile Madet <theo.madet@posteo.net>
2025-03-04 11:21:21 +0100

    Added TapirButton component and updated styling to fit the other buttons.

---
+++ src_frontend/components/TapirButton.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button, Spinner } from "react-bootstrap";
+
+interface TapirButtonProps {
+  variant: string;
+  text?: string;
+  icon?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  size?: "sm" | "lg" | undefined;
+  style?: React.CSSProperties;
+  loading?: boolean;
+  type?: "submit" | "reset" | "button" | undefined;
+}
+
+const TapirButton: React.FC<TapirButtonProps> = (props) => {
+  return (
+    <Button
+      variant={props.variant ?? "undefined"}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "6px",
+        ...props.style,
+      }}
+      size={props.size}
+      onClick={props.onClick}
+      disabled={props.disabled || props.loading}
+      type={props.type}
+    >
+      {props.icon &&
+        (props.loading ? (
+          <Spinner size="sm" />
+        ) : (
+          <span className={"material-icons"} style={{ fontSize: "16px" }}>
+            {props.icon}
+          </span>
+        ))}
+      {props.text && (
+        <h5 className={"mb-0"}>{props.loading ? "Loading..." : props.text}</h5>
+      )}
+    </Button>
+  );
+};
+
+export default TapirButton;
