bed9981f87768049ec40e9201b707a6d5c0b05ab
Théophile Madet <theo.madet@posteo.net>
2025-03-06 15:03:16 +0100

    Split GetDeliveriesService.get_deliveries in more subfunctions

--- tapir/deliveries/services/get_deliveries_service.py
+++ tapir/deliveries/services/get_deliveries_service.py
@@ -30,62 +30,65 @@ class GetDeliveriesService:
         if date_to is None and limit is None:
             limit = 10
 
+        iteration = 0
         next_delivery_date = get_next_delivery_date(date_from)
         while (date_to is not None and next_delivery_date <= date_to) or (
-            limit is not None and len(deliveries) < limit
+            limit is not None and iteration < limit
         ):
-            _, week_num, _ = next_delivery_date.isocalendar()
-            even_week = week_num % 2 == 0
+            delivery_object = cls.build_delivery_object(member, next_delivery_date)
+            if delivery_object:
+                deliveries.append(delivery_object)
 
-            active_subs = Subscription.objects.filter(
-                member=member,
-                start_date__lte=next_delivery_date,
-                end_date__gte=next_delivery_date,
-                product__type__delivery_cycle__in=[
-                    WEEKLY[0],
-                    EVEN_WEEKS[0] if even_week else ODD_WEEKS[0],
-                ],
-            )
-
-            if not active_subs.exists():
-                next_delivery_date = get_next_delivery_date(
-                    next_delivery_date + datetime.timedelta(days=1)
-                )
-                continue
-
-            pickup_location = member.get_pickup_location(next_delivery_date)
-            opening_times = PickupLocationOpeningTime.objects.filter(
-                pickup_location=pickup_location
-            )
-            next_delivery_date += datetime.timedelta(
-                days=(
-                    opening_times[0].day_of_week - next_delivery_date.weekday()
-                    if opening_times
-                    else 0
-                )
-            )
-
-            deliveries.append(
-                {
-                    "delivery_date": next_delivery_date,
-                    "pickup_location": pickup_location,
-                    "subscriptions": active_subs,
-                    "pickup_location_opening_times": opening_times,
-                    "joker_used": cls.is_joker_used_in_week(member, next_delivery_date),
-                    "can_joker_be_used": JokerManagementService.can_joker_be_used(
-                        member, next_delivery_date
-                    ),
-                }
-            )
-
-            if limit and len(deliveries) >= limit:
+            if limit and iteration >= limit:
                 break
 
             next_delivery_date = get_next_delivery_date(
                 next_delivery_date + datetime.timedelta(days=1)
             )
+            iteration += 1
 
         return deliveries
+
+    @classmethod
+    def build_delivery_object(cls, member: Member, delivery_date: datetime.date):
+        _, week_num, _ = delivery_date.isocalendar()
+        even_week = week_num % 2 == 0
+
+        active_subs = Subscription.objects.filter(
+            member=member,
+            start_date__lte=delivery_date,
+            end_date__gte=delivery_date,
+            product__type__delivery_cycle__in=[
+                WEEKLY[0],
+                EVEN_WEEKS[0] if even_week else ODD_WEEKS[0],
+            ],
+        )
+
+        if not active_subs.exists():
+            return None
+
+        pickup_location = member.get_pickup_location(delivery_date)
+        opening_times = PickupLocationOpeningTime.objects.filter(
+            pickup_location=pickup_location
+        )
+        delivery_date += datetime.timedelta(
+            days=(
+                opening_times[0].day_of_week - delivery_date.weekday()
+                if opening_times
+                else 0
+            )
+        )
+
+        return {
+            "delivery_date": delivery_date,
+            "pickup_location": pickup_location,
+            "subscriptions": active_subs,
+            "pickup_location_opening_times": opening_times,
+            "joker_used": cls.is_joker_used_in_week(member, delivery_date),
+            "can_joker_be_used": JokerManagementService.can_joker_be_used(
+                member, delivery_date
+            ),
+        }
 
     @classmethod
     def is_joker_used_in_week(
