5e0ccfe7e747bf2c6c935bfa105077c1712dab2b
Théophile Madet <theo.madet@posteo.net>
2025-03-11 17:13:28 +0100

    Removed debug log.

--- tapir/generic_exports/permissions.py
+++ tapir/generic_exports/permissions.py
@@ -1,4 +1,3 @@
-from icecream import ic
 from rest_framework import permissions
 
 from tapir.wirgarten.constants import Permission
@@ -6,5 +5,4 @@ from tapir.wirgarten.constants import Permission
 
 class HasCoopManagePermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        ic("has_permission", request.user.has_perm(Permission.Coop.MANAGE))
         return request.user.has_perm(Permission.Coop.MANAGE)
