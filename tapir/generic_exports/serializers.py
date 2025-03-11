7d12797870f7fde03f3ec413d774bb606bfbcf33
Théophile Madet <theo.madet@posteo.net>
2025-03-11 17:14:41 +0100

    Removed unused serializers

--- tapir/generic_exports/serializers.py
+++ tapir/generic_exports/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from tapir.generic_exports.models import CsvExport
-from tapir.generic_exports.services.export_segment_manager import ExportSegmentManager
 
 
 class ExportSegmentColumnSerializer(serializers.Serializer):
@@ -17,52 +16,10 @@ class ExportSegmentSerializer(serializers.Serializer):
     columns = ExportSegmentColumnSerializer(many=True)
 
 
-class CsvExportSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=False)
-    export_segment_id = serializers.CharField()
-    export_segment_name = serializers.CharField()
-    name = serializers.CharField()
-    description = serializers.CharField()
-    separator = serializers.CharField()
-    file_name = serializers.CharField()
-    columns = ExportSegmentColumnSerializer(many=True)
-    email_recipients = serializers.ListField(child=serializers.EmailField())
-
-
 class CsvExportModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = CsvExport
         fields = "__all__"
-
-
-class CreateCsvExportSerializer(serializers.Serializer):
-    export_segment_id = serializers.CharField()
-    name = serializers.CharField()
-    description = serializers.CharField()
-    separator = serializers.CharField()
-    file_name = serializers.CharField()
-    columns_ids = serializers.ListField(child=serializers.CharField())
-    email_recipients = serializers.ListField(child=serializers.EmailField())
-
-    @staticmethod
-    def validate(data):
-        if (
-            data["export_segment_id"]
-            not in ExportSegmentManager.registered_export_segments.keys()
-        ):
-            raise serializers.ValidationError(
-                f"Invalid export segment ID {data['export_segment_id']}"
-            )
-
-        segment = ExportSegmentManager.registered_export_segments[
-            data["export_segment_id"]
-        ]
-        column_ids = [column.id for column in segment.get_available_columns()]
-        for column_id in data["columns_ids"]:
-            if column_id not in column_ids:
-                raise serializers.ValidationError(f"Invalid column ID: {column_id}")
-
-        return data
 
 
 class BuildCsvExportResponseSerializer(serializers.Serializer):
