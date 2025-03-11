8406a20021d4e628b8b9684604d1afdf7691edce
Théophile Madet <theo.madet@posteo.net>
2025-03-11 18:04:51 +0100

    Added tests for AutomatedExportsManager.do_automated_exports

---
+++ tapir/generic_exports/tests/factories.py
@@ -0,0 +1,67 @@
+import factory
+from faker import Faker
+
+from tapir.generic_exports.models import CsvExport
+from tapir.generic_exports.services.export_segment_manager import ExportSegmentManager
+from tapir.wirgarten.models import ExportedFile
+
+fake = Faker()
+Faker.seed("tapir")
+
+
+class CsvExportFactory(factory.django.DjangoModelFactory[CsvExport]):
+    class Meta:
+        model = CsvExport
+
+    export_segment_id = factory.Faker(
+        "random_element",
+        elements=ExportSegmentManager.registered_export_segments.keys(),
+    )
+    name = factory.Faker("bs")
+    description = factory.Faker("bs")
+    separator = factory.Faker("random_element", elements=[",", ";", "-"])
+    automated_export_cycle = factory.Faker(
+        "random_element",
+        elements=[x[0] for x in CsvExport.AutomatedExportCycle.choices],
+    )
+    automated_export_day = factory.Faker("pyint", min_value=1, max_value=365)
+    automated_export_hour = factory.Faker("time")
+
+    @factory.post_generation
+    def email_recipients(self: CsvExport, create, email_recipients, **kwargs):
+        if not create:
+            return
+
+        if not email_recipients:
+            nb_recipients = fake.pyint(min_value=1, max_value=5)
+            email_recipients = [fake.email() for _ in range(nb_recipients)]
+
+        self.email_recipients = email_recipients
+
+    @factory.post_generation
+    def column_ids(self: CsvExport, create, column_ids, **kwargs):
+        if not create:
+            return
+
+        if not column_ids:
+            possible_ids = [
+                column.id
+                for column in ExportSegmentManager.registered_export_segments[
+                    self.export_segment_id
+                ].get_available_columns()
+            ]
+            column_ids = fake.random_elements(possible_ids, unique=True)
+
+        self.column_ids = column_ids
+
+
+class ExportedFileFactory(factory.django.DjangoModelFactory[ExportedFile]):
+    class Meta:
+        model = ExportedFile
+
+    name = factory.Faker("bs")
+    type = factory.Faker(
+        "random_element",
+        elements=[x[0] for x in ExportedFile.FileType.choices],
+    )
+    file = factory.Faker("binary")
---
+++ tapir/generic_exports/tests/services/automated_exports_manager/tests_do_automated_exports.py
@@ -0,0 +1,70 @@
+import datetime
+from unittest.mock import patch, Mock
+
+from tapir.generic_exports.models import CsvExport, AutomatedExportResult
+from tapir.generic_exports.services.automated_exports_manager import (
+    AutomatedExportsManager,
+)
+from tapir.generic_exports.tests.factories import CsvExportFactory, ExportedFileFactory
+from tapir.wirgarten.tests.test_utils import TapirIntegrationTest
+
+
+class TestDoAutomatedExports(TapirIntegrationTest):
+    def setUp(self) -> None:
+        pass
+
+    @patch.object(AutomatedExportsManager, "do_export")
+    @patch.object(AutomatedExportsManager, "get_datetime_of_latest_export")
+    def test_doAutomatedExports_exportHasCycleNever_noExportDone(
+        self, mock_get_datetime_of_latest_export: Mock, mock_do_export: Mock
+    ):
+        CsvExportFactory.create(
+            automated_export_cycle=CsvExport.AutomatedExportCycle.NEVER
+        )
+
+        AutomatedExportsManager.do_automated_exports()
+
+        mock_get_datetime_of_latest_export.assert_not_called()
+        mock_do_export.assert_not_called()
+
+    @patch.object(AutomatedExportsManager, "do_export")
+    @patch.object(AutomatedExportsManager, "get_datetime_of_latest_export")
+    def test_doAutomatedExports_alreadyExportedAtTargetDate_noExportDone(
+        self, mock_get_datetime_of_latest_export: Mock, mock_do_export: Mock
+    ):
+        export = CsvExportFactory.create(
+            automated_export_cycle=CsvExport.AutomatedExportCycle.DAILY
+        )
+        target_date = datetime.datetime(year=2023, month=12, day=5)
+        mock_get_datetime_of_latest_export.return_value = target_date
+        AutomatedExportResult.objects.create(
+            export_definition=export,
+            datetime=target_date,
+            file=ExportedFileFactory.create(),
+        )
+
+        AutomatedExportsManager.do_automated_exports()
+
+        mock_get_datetime_of_latest_export.assert_called_once_with(export)
+        mock_do_export.assert_not_called()
+
+    @patch.object(AutomatedExportsManager, "do_export")
+    @patch.object(AutomatedExportsManager, "get_datetime_of_latest_export")
+    def test_doAutomatedExports_noExportAtTargetDate_exportDone(
+        self, mock_get_datetime_of_latest_export: Mock, mock_do_export: Mock
+    ):
+        export = CsvExportFactory.create(
+            automated_export_cycle=CsvExport.AutomatedExportCycle.DAILY
+        )
+        target_date = datetime.datetime(year=2023, month=12, day=5)
+        mock_get_datetime_of_latest_export.return_value = target_date
+        AutomatedExportResult.objects.create(
+            export_definition=export,
+            datetime=target_date - datetime.timedelta(days=1),
+            file=ExportedFileFactory.create(),
+        )
+
+        AutomatedExportsManager.do_automated_exports()
+
+        mock_get_datetime_of_latest_export.assert_called_once_with(export)
+        mock_do_export.assert_called_once_with(export, target_date)
