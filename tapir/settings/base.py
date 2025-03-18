0528ee4f67eab6d762c699d9de9fb12462ee38f9
Théophile Madet <theo.madet@posteo.net>
2025-03-18 11:04:10 +0100

    Updated settings for mail module API

--- tapir/settings/base.py
+++ tapir/settings/base.py
@@ -206,3 +206,8 @@ DJANGO_VITE = {
         "manifest_path": "./dist/manifest.json",
     }
 }
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
+CORS_ALLOW_CREDENTIALS = True
