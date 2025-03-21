9b9d1e743f721efc8fad4cf07522ce362b5b839e
Théophile Madet <theo.madet@posteo.net>
2025-03-21 17:26:31 +0100

    Tests for MembershipCancellationManager.get_coop_entry_date

--- tapir/coop/services/membership_cancellation_manager.py
+++ tapir/coop/services/membership_cancellation_manager.py
@@ -7,20 +7,21 @@ from tapir.wirgarten.utils import get_today
 class MembershipCancellationManager:
     @classmethod
     def get_coop_entry_date(cls, member: Member):
-        try:
-            earliest_coopsharetransaction = member.coopsharetransaction_set.filter(
+        earliest_transaction = (
+            member.coopsharetransaction_set.filter(
                 transaction_type__in=[
                     CoopShareTransaction.CoopShareTransactionType.PURCHASE,
                     CoopShareTransaction.CoopShareTransactionType.TRANSFER_IN,
                 ]
-            ).earliest("valid_at")
-            return (
-                earliest_coopsharetransaction.valid_at
-                if earliest_coopsharetransaction
-                else None
             )
-        except CoopShareTransaction.DoesNotExist:
+            .order_by("valid_at")
+            .first()
+        )
+
+        if not earliest_transaction:
             return None
+
+        return earliest_transaction.valid_at
 
     @classmethod
     def can_member_cancel_coop_membership(
