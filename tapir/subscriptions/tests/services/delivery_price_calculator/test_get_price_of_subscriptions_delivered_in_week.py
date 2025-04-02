b53756adb06ba804aec2f76d0631fc18eafafa55
Théophile Madet <theo.madet@posteo.net>
2025-04-02 11:35:28 +0200

    Test fixes

--- tapir/subscriptions/tests/services/delivery_price_calculator/test_get_price_of_subscriptions_delivered_in_week.py
+++ tapir/subscriptions/tests/services/delivery_price_calculator/test_get_price_of_subscriptions_delivered_in_week.py
@@ -8,20 +8,22 @@ from tapir.subscriptions.services.delivery_price_calculator import (
 
 
 class TestGetPriceOfSubscriptionsDeliveredInWeek(SimpleTestCase):
-    @patch.object(DeliveryPriceCalculator, "get_price_of_single_delivery")
+    @patch.object(
+        DeliveryPriceCalculator, "get_price_of_single_delivery_without_solidarity"
+    )
     @patch.object(
         DeliveryPriceCalculator, "get_subscriptions_that_get_delivered_in_week"
     )
     def test_getPriceOfSubscriptionsDeliveredInWeek_default_returnsSumOfPriceTimesQuantity(
         self,
         mock_get_subscriptions_that_get_delivered_in_week: Mock,
-        mock_get_price_of_single_delivery: Mock,
+        mock_get_price_of_single_delivery_without_solidarity: Mock,
     ):
         subscriptions = [Mock(), Mock()]
         subscriptions[0].quantity = 2
         subscriptions[1].quantity = 4
         mock_get_subscriptions_that_get_delivered_in_week.return_value = subscriptions
-        mock_get_price_of_single_delivery.side_effect = [15, 26]
+        mock_get_price_of_single_delivery_without_solidarity.side_effect = [15, 26]
         member = Mock()
         reference_date = Mock()
 
@@ -34,8 +36,10 @@ class TestGetPriceOfSubscriptionsDeliveredInWeek(SimpleTestCase):
         mock_get_subscriptions_that_get_delivered_in_week.assert_called_once_with(
             member, reference_date
         )
-        self.assertEqual(2, mock_get_price_of_single_delivery.call_count)
-        mock_get_price_of_single_delivery.assert_has_calls(
+        self.assertEqual(
+            2, mock_get_price_of_single_delivery_without_solidarity.call_count
+        )
+        mock_get_price_of_single_delivery_without_solidarity.assert_has_calls(
             [call(subscription, reference_date) for subscription in subscriptions],
             any_order=True,
         )
