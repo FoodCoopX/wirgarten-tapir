afa9ec34327e7f011f8e99b38373c2c2774bae09
Théophile Madet <theo.madet@posteo.net>
2025-04-04 12:20:46 +0200

    Updated tests for get_capacity_usage_at_date

--- tapir/pickup_locations/tests/services/pickup_location_capacity_mode_share_checker/test_get_capacity_usage_at_date.py
+++ tapir/pickup_locations/tests/services/pickup_location_capacity_mode_share_checker/test_get_capacity_usage_at_date.py
@@ -1,10 +1,12 @@
 import datetime
+from unittest.mock import patch, Mock
 
+from tapir.configuration.models import TapirParameter
 from tapir.pickup_locations.services.pickup_location_capacity_mode_share_checker import (
     PickupLocationCapacityModeShareChecker,
 )
-from tapir.wirgarten.models import MemberPickupLocation, Subscription
-from tapir.wirgarten.parameters import ParameterDefinitions
+from tapir.wirgarten.models import MemberPickupLocation, Subscription, GrowingPeriod
+from tapir.wirgarten.parameters import ParameterDefinitions, Parameter
 from tapir.wirgarten.tests.factories import (
     MemberFactory,
     PickupLocationFactory,
@@ -13,6 +15,7 @@ from tapir.wirgarten.tests.factories import (
     SubscriptionFactory,
     ProductPriceFactory,
     ProductFactory,
+    GrowingPeriodFactory,
 )
 from tapir.wirgarten.tests.test_utils import TapirIntegrationTest
 
@@ -31,11 +34,15 @@ class TestGetCapacityUsageAtDate(TapirIntegrationTest):
         )
         cls.product_type = ProductTypeFactory.create()
         product = ProductFactory.create(type=cls.product_type)
+        start_date = cls.reference_date - datetime.timedelta(days=1)
+        end_date = cls.reference_date + datetime.timedelta(days=1)
         SubscriptionFactory.create(
             product=product,
             member=member,
-            start_date=cls.reference_date - datetime.timedelta(days=1),
-            end_date=cls.reference_date + datetime.timedelta(days=1),
+            start_date=start_date,
+            end_date=end_date,
+            period__start_date=start_date,
+            period__end_date=end_date,
             quantity=3,
         )
         ProductPriceFactory.create(
@@ -78,3 +85,52 @@ class TestGetCapacityUsageAtDate(TapirIntegrationTest):
         )
 
         self.assertEqual(6, result)
+
+    def test_getCapacityUsageAtDate_subscriptionWillBeRenewed_pastSubscriptionCounted(
+        self,
+    ):
+        TapirParameter.objects.filter(
+            key=Parameter.SUBSCRIPTION_AUTOMATIC_RENEWAL
+        ).update(value=True)
+        current_growing_period = GrowingPeriod.objects.get()
+        past_growing_period = GrowingPeriodFactory.create(
+            start_date=current_growing_period.start_date - datetime.timedelta(days=2),
+            end_date=current_growing_period.start_date - datetime.timedelta(days=1),
+        )
+        Subscription.objects.update(
+            period=current_growing_period,
+            start_date=past_growing_period.start_date,
+            end_date=past_growing_period.end_date,
+        )
+        result = PickupLocationCapacityModeShareChecker.get_capacity_usage_at_date(
+            self.pickup_location, self.product_type, self.reference_date
+        )
+
+        self.assertEqual(6, result)
+
+    @patch.object(
+        PickupLocationCapacityModeShareChecker,
+        "extend_subscriptions_with_those_that_will_be_renewed",
+    )
+    def test_getCapacityUsageAtDate_subscriptionWillNotRenewedBecauseOfParameter_pastSubscriptionNotCounted(
+        self, mock_extend_subscriptions_with_those_that_will_be_renewed: Mock()
+    ):
+        TapirParameter.objects.filter(
+            key=Parameter.SUBSCRIPTION_AUTOMATIC_RENEWAL
+        ).update(value=False)
+        current_growing_period = GrowingPeriod.objects.get()
+        past_growing_period = GrowingPeriodFactory.create(
+            start_date=current_growing_period.start_date - datetime.timedelta(days=2),
+            end_date=current_growing_period.start_date - datetime.timedelta(days=1),
+        )
+        Subscription.objects.update(
+            period=current_growing_period,
+            start_date=past_growing_period.start_date,
+            end_date=past_growing_period.end_date,
+        )
+        result = PickupLocationCapacityModeShareChecker.get_capacity_usage_at_date(
+            self.pickup_location, self.product_type, self.reference_date
+        )
+
+        self.assertEqual(0, result)
+        mock_extend_subscriptions_with_those_that_will_be_renewed.assert_not_called()
