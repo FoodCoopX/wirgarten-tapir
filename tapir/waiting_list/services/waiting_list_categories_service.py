1550cdf8bc12902b83441135695e366e4f5c70a5
Théophile Madet <theo.madet@posteo.net>
2025-05-08 16:43:40 +0200

    Added tests for WaitingListCategoriesService

--- tapir/waiting_list/services/waiting_list_categories_service.py
+++ tapir/waiting_list/services/waiting_list_categories_service.py
@@ -10,6 +10,9 @@ class WaitingListCategoriesService:
         base_string = get_parameter_value(
             ParameterKeys.SUBSCRIPTION_WAITING_LIST_CATEGORIES, cache=cache
         )
+        base_string = base_string.strip()
+        if base_string == "":
+            return []
         categories = base_string.split(",")
         categories = [category.strip() for category in categories]
         return categories
