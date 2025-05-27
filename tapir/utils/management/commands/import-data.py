7dcc79ed87fa2a5b49fb7612c57920b4e0a85d18
dev@kirchner-bln.de <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>
2025-05-27 10:51:19 +0200

    Fix alignment of concatenated timestamps in import-data.py
    Adjusted the whitespace for better readability and consistency in timestamp concatenation. This change ensures the code adheres to standard formatting practices and improves maintainability.

--- tapir/utils/management/commands/import-data.py
+++ tapir/utils/management/commands/import-data.py
@@ -278,9 +278,9 @@ class Command(BaseCommand):
                             period_id=period.id,
                             product_id=prod.id,
                             consent_ts=row["consent_vertragsgrundsätze"]
-                            + " 12:00+0200",
+                                       + " 12:00+0200",
                             withdrawal_consent_ts=row["consent_widerruf"]
-                            + " 12:00+0200",
+                                                  + " 12:00+0200",
                         )
                         # print("Subscription object successfully created.")
                     except django.db.Error as e:
