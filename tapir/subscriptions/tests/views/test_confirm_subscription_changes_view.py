4fc6c8f6e46cf184f720bf0e934491fe219e3b69
Théophile Madet <theo.madet@posteo.net>
2025-06-02 13:37:42 +0200

    Extended tests for confirmations with share purchases.

--- tapir/subscriptions/tests/views/test_confirm_subscription_changes_view.py
+++ tapir/subscriptions/tests/views/test_confirm_subscription_changes_view.py
@@ -3,14 +3,22 @@ from rest_framework import status
 
 from tapir.wirgarten.models import Subscription
 from tapir.wirgarten.parameters import ParameterDefinitions
-from tapir.wirgarten.tests.factories import MemberFactory, SubscriptionFactory, NOW
-from tapir.wirgarten.tests.test_utils import TapirIntegrationTest
+from tapir.wirgarten.tests.factories import (
+    MemberFactory,
+    SubscriptionFactory,
+    NOW,
+    CoopShareTransactionFactory,
+)
+from tapir.wirgarten.tests.test_utils import TapirIntegrationTest, mock_timezone
 
 
 class TestConfirmSubscriptionChangesView(TapirIntegrationTest):
     @classmethod
     def setUpTestData(cls):
         ParameterDefinitions().import_definitions()
+
+    def setUp(self) -> None:
+        self.now = mock_timezone(self, NOW)
 
     def test_post_loggedInAsNormalMember_returns403(self):
         self.client.force_login(MemberFactory.create())
@@ -24,9 +32,9 @@ class TestConfirmSubscriptionChangesView(TapirIntegrationTest):
         self,
     ):
         self.client.force_login(MemberFactory.create(is_superuser=True))
-        subscription_1 = SubscriptionFactory.create(cancellation_ts=NOW)
+        subscription_1 = SubscriptionFactory.create(cancellation_ts=self.now)
         subscription_2 = SubscriptionFactory.create(
-            cancellation_ts=NOW, cancellation_admin_confirmed=NOW
+            cancellation_ts=self.now, cancellation_admin_confirmed=self.now
         )
 
         url = reverse("subscriptions:confirm_subscription_changes")
@@ -41,11 +49,29 @@ class TestConfirmSubscriptionChangesView(TapirIntegrationTest):
         self,
     ):
         self.client.force_login(MemberFactory.create(is_superuser=True))
-        subscription_1 = SubscriptionFactory.create(cancellation_ts=NOW)
-        subscription_2 = SubscriptionFactory.create(admin_confirmed=NOW)
+        subscription_1 = SubscriptionFactory.create(cancellation_ts=self.now)
+        subscription_2 = SubscriptionFactory.create(admin_confirmed=self.now)
 
         url = reverse("subscriptions:confirm_subscription_changes")
         url = f"{url}?confirm_cancellation_ids={subscription_1.id}&confirm_creation_ids={subscription_2.id}"
+        response = self.client.post(url)
+
+        self.assertStatusCode(response, status.HTTP_404_NOT_FOUND)
+        self.assertFalse(
+            Subscription.objects.filter(
+                cancellation_admin_confirmed__isnull=False
+            ).exists()
+        )
+
+    def test_post_someCoopPurchaseIdsDontExist_returns404AndDontConfirmAnything(
+        self,
+    ):
+        self.client.force_login(MemberFactory.create(is_superuser=True))
+        subscription = SubscriptionFactory.create(admin_confirmed=None)
+        purchase = CoopShareTransactionFactory.create(admin_confirmed=self.now)
+
+        url = reverse("subscriptions:confirm_subscription_changes")
+        url = f"{url}?confirm_creation_ids={subscription.id}&confirm_purchase_ids={purchase.id}"
         response = self.client.post(url)
 
         self.assertStatusCode(response, status.HTTP_404_NOT_FOUND)
@@ -59,16 +85,19 @@ class TestConfirmSubscriptionChangesView(TapirIntegrationTest):
         self.client.force_login(MemberFactory.create(is_superuser=True))
 
         subscription_1 = SubscriptionFactory.create(
-            cancellation_ts=NOW, admin_confirmed=NOW
+            cancellation_ts=self.now, admin_confirmed=self.now
         )
         subscription_2 = SubscriptionFactory.create()
+        purchase = CoopShareTransactionFactory.create(admin_confirmed=None)
 
         url = reverse("subscriptions:confirm_subscription_changes")
-        url = f"{url}?confirm_cancellation_ids={subscription_1.id}&confirm_creation_ids={subscription_2.id}"
+        url = f"{url}?confirm_cancellation_ids={subscription_1.id}&confirm_creation_ids={subscription_2.id}&confirm_purchase_ids={purchase.id}"
         response = self.client.post(url)
 
         self.assertStatusCode(response, status.HTTP_200_OK)
         subscription_1.refresh_from_db()
+        self.assertEqual(self.now, subscription_1.cancellation_admin_confirmed)
         subscription_2.refresh_from_db()
-        self.assertIsNotNone(subscription_1.cancellation_admin_confirmed)
-        self.assertIsNotNone(subscription_2.admin_confirmed)
+        self.assertIsNotNone(self.now, subscription_2.admin_confirmed)
+        purchase.refresh_from_db()
+        self.assertIsNotNone(self.now, purchase.admin_confirmed)
