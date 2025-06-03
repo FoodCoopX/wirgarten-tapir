d0044561dbc59ee9a8d019d17d0aeea9add7bf61
Théophile Madet <theo.madet@posteo.net>
2025-06-03 18:08:18 +0200

    Removed theme-specific components

--- src_frontend/bestell_wizard/BestellWizardProductType.tsx
+++ src_frontend/bestell_wizard/BestellWizardProductType.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { TapirTheme } from "../types/TapirTheme.ts";
-import BestellWizardProductTypeBiotop from "./biotop/BestellWizardProductTypeBiotop.tsx";
 import { PublicProductType } from "../api-client";
 import { ShoppingCart } from "./ShoppingCart.ts";
+import { Col, Row } from "react-bootstrap";
+import { BESTELL_WIZARD_COLUMN_SIZE } from "./BESTELL_WIZARD_COLUMN_SIZE.ts";
+import ProductForm from "./ProductForm.tsx";
+import TapirButton from "../components/TapirButton.tsx";
 
 interface BestellWizardProductTypeProps {
   theme: TapirTheme;
@@ -21,17 +24,55 @@ const BestellWizardProductType: React.FC<BestellWizardProductTypeProps> = ({
   shoppingCart,
   setShoppingCart,
 }) => {
+  function getHtmlDescription(description: string) {
+    return { __html: description };
+  }
+
   return (
     <>
-      {theme === "biotop" && (
-        <BestellWizardProductTypeBiotop
-          productType={productType}
-          onProductTypeNextClicked={onProductTypeNextClicked}
-          onProductTypePreviousClicked={onProductTypePreviousClicked}
-          shoppingCart={shoppingCart}
-          setShoppingCart={setShoppingCart}
-        />
-      )}
+      <Row className={"justify-content-center"}>
+        <Col sm={BESTELL_WIZARD_COLUMN_SIZE}>
+          <h1>{productType.name}</h1>
+          {
+            <span
+              dangerouslySetInnerHTML={getHtmlDescription(
+                productType.descriptionBestellwizardLong!,
+              )}
+            ></span>
+          }
+        </Col>
+      </Row>
+      <Row className={"justify-content-center"}>
+        <Col sm={BESTELL_WIZARD_COLUMN_SIZE}>
+          <ProductForm
+            productType={productType}
+            shoppingCart={shoppingCart}
+            setShoppingCart={setShoppingCart}
+          />
+        </Col>
+      </Row>
+      <Row className={"justify-content-center mt-4 mb-2"}>
+        <Col sm={BESTELL_WIZARD_COLUMN_SIZE}>
+          <Row className={"justify-content-between"}>
+            <Col>
+              <TapirButton
+                icon={"arrow_backward"}
+                variant={"outline-primary"}
+                text={"Zurück"}
+                onClick={onProductTypePreviousClicked}
+              />
+            </Col>
+            <Col className={"d-flex justify-content-end"}>
+              <TapirButton
+                icon={"arrow_forward"}
+                variant={"outline-primary"}
+                text={"Weiter"}
+                onClick={onProductTypeNextClicked}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </>
   );
 };
