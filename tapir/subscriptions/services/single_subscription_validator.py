a6ed87cacf8414346e34a054ca710dbd4b566030
Théophile Madet <theo.madet@posteo.net>
2025-06-17 12:51:19 +0200

    WIP validation of single subscription product types

---
+++ tapir/subscriptions/services/single_subscription_validator.py
@@ -0,0 +1,26 @@
+from tapir.subscriptions.types import TapirOrder
+from tapir.utils.services.tapir_cache import TapirCache
+
+
+class SingleSubscriptionValidator:
+    @classmethod
+    def validate_single_subscription_products_are_ordered_at_most_once(
+        cls, order: TapirOrder, cache: dict
+    ):
+        ordered_product_type_ids = {product.type_id for product in order.keys()}
+        for product_type_id in ordered_product_type_ids:
+            product_type = TapirCache.get_product_type_by_id(
+                cache=cache, product_type_id=product_type_id
+            )
+            if product_type.single_subscription_only:
+                continue
+
+            total_quantity = 0
+            for product, quantity in order.items():
+                if product.type_id == product_type_id:
+                    total_quantity += quantity
+
+            if total_quantity > 1:
+                return False
+
+        return True
