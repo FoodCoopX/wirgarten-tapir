06fff49d492a25585503715c665a28a1aace17d0
Théophile Madet <theo.madet@posteo.net>
2025-08-18 10:48:06 +0200

    US 2.3: Updated service name in payment task

--- tapir/payments/tasks.py
+++ tapir/payments/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 
-from tapir.payments.services.month_payment_generator import MonthPaymentGenerator
+from tapir.payments.services.month_payment_builder import MonthPaymentBuilder
 from tapir.wirgarten.models import (
     Payment,
 )
@@ -13,7 +13,7 @@ from tapir.wirgarten.utils import (
 def create_payments_for_this_month():
     cache = {}
     today = get_today(cache=cache)
-    payments = MonthPaymentGenerator.build_payments_for_month(
+    payments = MonthPaymentBuilder.build_payments_for_month(
         reference_date=today, cache=cache
     )
     Payment.objects.bulk_create(payments)
