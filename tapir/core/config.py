49c11714014774b3ace05e9e20c4243d79b0a4fe
Théophile Madet <theo.madet@posteo.net>
2025-09-03 16:00:10 +0200

    Added theme for GrüneNeune

--- tapir/core/config.py
+++ tapir/core/config.py
@@ -30,10 +30,12 @@ THEME_BIOTOP = "biotop"
 THEME_WIRGARTEN = "wirgarten"
 THEME_L2G = "l2g"
 THEME_MM = "mm"
+THEME_G9 = "g9"
 
 THEME_OPTIONS = [
     (THEME_BIOTOP, "Biotop Oberland"),
     (THEME_WIRGARTEN, "WirGarten Lüneburg"),
     (THEME_L2G, "L2G Dickendorf"),
     (THEME_MM, "Möllers Morgen"),
+    (THEME_G9, "GrüneNeune"),
 ]
