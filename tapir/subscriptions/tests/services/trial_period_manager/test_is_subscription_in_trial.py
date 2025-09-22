02af323621522eda4a4379b608625f1640c65e04
Théophile Madet <theo.madet@posteo.net>
2025-09-22 20:54:39 +0200

    US 2.3.: Subscriptions that got cancelled can still be on trial

--- tapir/subscriptions/tests/services/trial_period_manager/test_is_subscription_in_trial.py
+++ tapir/subscriptions/tests/services/trial_period_manager/test_is_subscription_in_trial.py
@@ -77,27 +77,6 @@ class TestIsSubscriptionInTrial(SimpleTestCase):
 
     @patch("tapir.subscriptions.services.trial_period_manager.get_parameter_value")
     @patch.object(TrialPeriodManager, "get_end_of_trial_period")
-    def test_isSubscriptionInTrial_subscriptionHasBeenCancelled_returnsFalse(
-        self, mock_get_end_of_trial_period: Mock, mock_get_parameter_value: Mock
-    ):
-        mock_get_end_of_trial_period.return_value = datetime.date(
-            year=2026, month=3, day=15
-        )
-        subscription = Mock()
-        subscription.cancellation_ts = datetime.datetime(year=2026, month=3, day=15)
-        mock_get_parameter_value.return_value = True
-        cache = {}
-
-        result = TrialPeriodManager.is_subscription_in_trial(subscription, cache=cache)
-
-        self.assertFalse(result)
-        mock_get_parameter_value.assert_called_once_with(
-            ParameterKeys.TRIAL_PERIOD_ENABLED, cache=cache
-        )
-        mock_get_end_of_trial_period.assert_not_called()
-
-    @patch("tapir.subscriptions.services.trial_period_manager.get_parameter_value")
-    @patch.object(TrialPeriodManager, "get_end_of_trial_period")
     def test_isSubscriptionInTrial_trialPeriodsAreDisabled_returnsFalse(
         self, mock_get_end_of_trial_period: Mock, mock_get_parameter_value: Mock
     ):
