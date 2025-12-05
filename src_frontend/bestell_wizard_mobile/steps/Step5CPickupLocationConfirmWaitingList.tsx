48b5fe75688843634351d25ba2891511bac73c99
Théophile Madet <theo.madet@posteo.net>
2025-12-05 12:04:12 +0100

    US 5.8 #742: Simplified buttons on the pickup location confirm step.

--- src_frontend/bestell_wizard_mobile/steps/Step5CPickupLocationConfirmWaitingList.tsx
+++ src_frontend/bestell_wizard_mobile/steps/Step5CPickupLocationConfirmWaitingList.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BestellWizardSettings } from "../../bestell_wizard/types/BestellWizardSettings.ts";
-import NextStepButton from "../components/NextStepButton.tsx";
 import { BUTTON_VARIANT } from "../utils/BUTTON_VARIANT.ts";
 import TapirButton from "../../components/TapirButton.tsx";
 import { Step } from "../types/Step.ts";
@@ -19,19 +18,23 @@ const Step5BPickupLocationConfirmWaitingList: React.FC<
   return (
     <>
       <p dangerouslySetInnerHTML={{ __html: settings.strings.step5cText }} />
-      <TapirButton
-        variant={BUTTON_VARIANT}
-        text={"Weitere Wünsche eintragen"}
-        icon={"keyboard_arrow_up"}
-        onClick={() => {
-          setCurrentStep("5b_pickup_location_choice");
-          setCurrentTab("wishes");
-        }}
-      />
-      <NextStepButton
-        onClick={goToNextStep}
-        text={"Mit nur einen Wunsch weitergehen"}
-      />
+      <div className={"d-flex flex-row gap-2"}>
+        <TapirButton
+          variant={BUTTON_VARIANT}
+          text={"Ja"}
+          icon={"keyboard_arrow_up"}
+          onClick={() => {
+            setCurrentStep("5b_pickup_location_choice");
+            setCurrentTab("wishes");
+          }}
+        />
+        <TapirButton
+          onClick={goToNextStep}
+          text={"Nein"}
+          variant={BUTTON_VARIANT}
+          icon={"keyboard_arrow_down"}
+        />
+      </div>
     </>
   );
 };
