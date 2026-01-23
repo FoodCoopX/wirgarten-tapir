6b44c8809c4dce13fb5090d54ef51dd77f914364
Théophile Madet <theo.madet@posteo.net>
2026-01-23 10:31:22 +0100

    fix: avoid error in bestell wizard when there is only one future growing period.

--- tapir/subscriptions/services/growing_period_choice_provider.py
+++ tapir/subscriptions/services/growing_period_choice_provider.py
@@ -5,26 +5,25 @@ from tapir.subscriptions.services.contract_start_date_calculator import (
     ContractStartDateCalculator,
 )
 from tapir.utils.services.tapir_cache import TapirCache
+from tapir.wirgarten.models import GrowingPeriod
 from tapir.wirgarten.parameter_keys import ParameterKeys
 from tapir.wirgarten.utils import get_today
 
 
 class GrowingPeriodChoiceProvider:
-    @staticmethod
-    def get_available_growing_periods(reference_date: datetime.date, cache: dict):
-        growing_periods = TapirCache.get_all_growing_periods_ascending(cache=cache)
+    @classmethod
+    def get_available_growing_periods(cls, reference_date: datetime.date, cache: dict):
+        all_growing_periods = TapirCache.get_all_growing_periods_ascending(cache=cache)
         start_date_threshold = get_today(cache=cache) + datetime.timedelta(
             days=get_parameter_value(
                 key=ParameterKeys.ENABLE_GROWING_PERIOD_CHOICE_DAYS_BEFORE
             )
         )
-        growing_periods = [
-            growing_period
-            for growing_period in growing_periods
-            if growing_period.end_date >= reference_date
-            and growing_period.is_available_in_bestell_wizard
-            and growing_period.start_date <= start_date_threshold
-        ][:2]
+        growing_periods = cls.filter_periods(
+            growing_periods=all_growing_periods,
+            reference_date=reference_date,
+            threshold=start_date_threshold,
+        )[:2]
 
         if len(growing_periods) == 2:
             contract_start_current_period = ContractStartDateCalculator.get_next_contract_start_date_in_growing_period(
@@ -36,4 +35,26 @@ class GrowingPeriodChoiceProvider:
             if contract_start_next_period == contract_start_current_period:
                 growing_periods = [growing_periods[1]]
 
+        if len(growing_periods) == 0:
+            growing_periods = cls.filter_periods(
+                growing_periods=all_growing_periods,
+                reference_date=reference_date,
+                threshold=None,
+            )[:1]
+
         return growing_periods
+
+    @classmethod
+    def filter_periods(
+        cls,
+        growing_periods: list[GrowingPeriod],
+        reference_date: datetime.date,
+        threshold: datetime.date | None,
+    ):
+        return [
+            growing_period
+            for growing_period in growing_periods
+            if growing_period.end_date >= reference_date
+            and growing_period.is_available_in_bestell_wizard
+            and (threshold is None or growing_period.start_date <= threshold)
+        ][:2]
--- tapir/subscriptions/tests/services/test_growing_period_choice_provider.py
+++ tapir/subscriptions/tests/services/test_growing_period_choice_provider.py
@@ -137,3 +137,19 @@ class TestGrowingPeriodChoiceProvider(TapirIntegrationTest):
         )
 
         self.assertEqual([future_growing_period], result)
+
+    def test_getAvailableGrowingPeriods_onlyExistingPeriodIsInTheFuture_futurePeriodReturnedIgnoringThreshold(
+        self,
+    ):
+        now = mock_timezone(self, now=datetime.datetime(year=2000, month=1, day=1))
+        future_growing_period = GrowingPeriodFactory.create(
+            start_date=datetime.date(year=2001, month=1, day=1),
+            is_available_in_bestell_wizard=True,
+        )
+        cache = {}
+
+        result = GrowingPeriodChoiceProvider.get_available_growing_periods(
+            reference_date=now.date(), cache=cache
+        )
+
+        self.assertEqual([future_growing_period], result)
