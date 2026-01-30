3737aa0632b5e61e358c05bd94be29f4621b5f8f
oliverpool <git@olivier.pfad.fr>
2026-01-30 09:31:12 +0100

    backend: implement column provider for GenG export

--- tapir/generic_exports/services/member_column_provider.py
+++ tapir/generic_exports/services/member_column_provider.py
@@ -4,14 +4,24 @@ import datetime
 import locale
 from typing import TYPE_CHECKING, Dict
 
+from django.db.models import (
+    F,
+    Max,
+    Min,
+    Sum,
+)
+
 from tapir.generic_exports.services.export_segment_manager import ExportSegmentColumn
 from tapir.subscriptions.services.delivery_price_calculator import (
     DeliveryPriceCalculator,
 )
 from tapir.utils.services.tapir_cache import TapirCache
+from tapir.utils.user_utils import UserUtils
 
 if TYPE_CHECKING:
-    from tapir.wirgarten.models import Member
+    from tapir.wirgarten.models import (
+        Member,
+    )
 
 
 class MemberColumnProvider:
@@ -78,6 +88,36 @@ class MemberColumnProvider:
                 display_name="Joker Gutschrift Details",
                 description="Gutschrift [Anzahl genutzte Joker] Joker in [Vertragsjahr]",
                 get_value=cls.get_value_member_joker_credit_details,
+            ),
+            ExportSegmentColumn(
+                id="member_full_address",
+                display_name="Anschrift",
+                description="",
+                get_value=cls.get_value_member_full_address,
+            ),
+            ExportSegmentColumn(
+                id="member_share_quantity",
+                display_name="Anzahl Anteile",
+                description="",
+                get_value=cls.get_value_member_share_quantity,
+            ),
+            ExportSegmentColumn(
+                id="member_admission_date",
+                display_name="Beitrittsdatum",
+                description="",
+                get_value=cls.get_value_member_admission_date,
+            ),
+            ExportSegmentColumn(
+                id="member_termination_date",
+                display_name="Austrittsdatum",
+                description="",
+                get_value=cls.get_value_member_termination_date,
+            ),
+            ExportSegmentColumn(
+                id="member_share_history",
+                display_name="Anteilshistorie",
+                description="",
+                get_value=cls.get_value_member_share_history,
             ),
         ]
 
@@ -157,3 +197,50 @@ class MemberColumnProvider:
         date_from = growing_period.start_date.strftime("%d.%m.%Y")
         date_to = reference_datetime.strftime("%d.%m.%Y")
         return f"Gutschrift {nb_jokers} genutzte Joker in Vertragsjahr {date_from}-{date_to}"
+
+    @classmethod
+    def get_value_member_full_address(cls, member: Member, _, __):
+        return UserUtils.build_display_address(
+            member.street, member.street_2, member.postcode, member.city
+        )
+
+    @classmethod
+    def get_value_member_share_quantity(
+        cls, member: Member, reference_datetime: datetime.datetime, _
+    ):
+        return (
+            member.coopsharetransaction_set.filter(
+                valid_at__lte=reference_datetime
+            ).aggregate(quantity=Sum(F("quantity")))["quantity"]
+            or 0
+        )
+
+    @classmethod
+    def get_value_member_share_history(
+        cls, member: Member, reference_datetime: datetime.datetime, _
+    ):
+        return "\n".join(
+            f"{transaction.quantity:+} am {transaction.valid_at.strftime('%d.%m.%Y')}"
+            for transaction in member.coopsharetransaction_set.filter(
+                valid_at__lte=reference_datetime
+            ).order_by("valid_at")
+        )
+
+    @classmethod
+    def get_value_member_admission_date(cls, member: Member, _, __):
+        min_valid_at = member.coopsharetransaction_set.aggregate(
+            min_valid_at=Min("valid_at")
+        )["min_valid_at"]
+        if not min_valid_at:
+            return ""
+        return min_valid_at.strftime("%d.%m.%Y")
+
+    @classmethod
+    def get_value_member_termination_date(cls, member: Member, _, __):
+        agg = member.coopsharetransaction_set.aggregate(
+            max_valid_at=Max("valid_at"),
+            quantity=Sum(F("quantity")),
+        )
+        if agg["quantity"] or not agg["max_valid_at"]:
+            return ""
+        return agg["max_valid_at"].strftime("%d.%m.%Y")
