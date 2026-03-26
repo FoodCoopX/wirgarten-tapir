9cbd2d8e207ad7769374f18262053876595cdbe3
Théophile Madet <theo.madet@posteo.net>
2026-03-26 11:06:40 +0100

    feat: #977 added an explanation text to the payments modal.

---
+++ src_frontend/components/TapirHelpButton.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from "react";
+import { OverlayTrigger, Popover } from "react-bootstrap";
+import TapirButton from "./TapirButton.tsx";
+
+interface TapirHelpButtonProps {
+  title?: string;
+  text: ReactNode;
+  buttonSize?: "sm" | "lg";
+  width?: string;
+}
+
+const TapirHelpButton: React.FC<TapirHelpButtonProps> = ({
+  title,
+  text,
+  buttonSize,
+  width,
+}) => {
+  function buildTooltip() {
+    return (
+      <Popover id="popover-basic" style={{ maxWidth: width ?? "500px" }}>
+        <Popover.Header as="h3">{title ?? "Erklärung"}</Popover.Header>
+        <Popover.Body>{text}</Popover.Body>
+      </Popover>
+    );
+  }
+
+  return (
+    <OverlayTrigger
+      overlay={buildTooltip()}
+      trigger={"click"}
+      placement={"bottom"}
+    >
+      <TapirButton
+        variant={"outline-secondary"}
+        icon={"help"}
+        size={buttonSize}
+      />
+    </OverlayTrigger>
+  );
+};
+
+export default TapirHelpButton;
