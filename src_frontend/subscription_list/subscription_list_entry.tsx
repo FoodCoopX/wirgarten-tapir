4df944df0b41c64470ce06e2abb6a2414bce7a82
Noa <noa_filou+github@disroot.org>
2026-04-08 13:15:51 +0200

    Added another Button to subscription_list by copying SubscriptionChangeDatesButton.tsx and started adjusting it for adding contracts (Issue #1037)

--- src_frontend/subscription_list/subscription_list_entry.tsx
+++ src_frontend/subscription_list/subscription_list_entry.tsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
 import SubscriptionChangeDatesButton from "./SubscriptionChangeDatesButton.tsx";
+import SubscriptionAddContractButton from "./SubscriptionAddContractButton.tsx";
 import { getCsrfToken } from "../utils/getCsrfToken.ts";
 
 const domNodeSubscriptionChangeDatesButton = document.getElementById(
   "subscription_change_dates_button",
+);
+const domNodeSubscriptionAddContractButton = document.getElementById(
+    "subscription_add_contract_button",
 );
 
 if (!domNodeSubscriptionChangeDatesButton) {
@@ -11,4 +15,11 @@ if (!domNodeSubscriptionChangeDatesButton) {
 } else {
   const root = createRoot(domNodeSubscriptionChangeDatesButton);
   root.render(<SubscriptionChangeDatesButton csrfToken={getCsrfToken()} />);
+}
+
+if (!domNodeSubscriptionAddContractButton) {
+  console.error("Subscription add contract button not found");
+} else {
+  const root = createRoot(domNodeSubscriptionAddContractButton);
+  root.render(<SubscriptionAddContractButton csrfToken={getCsrfToken()} />);
 }
