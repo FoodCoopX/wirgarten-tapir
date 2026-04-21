16e494d0897b28bd4a62bd1f77d8271b79718435
Théophile Madet <theo.madet@posteo.net>
2026-04-21 13:38:01 +0200

    dev: added a helper script to down all docker containers and recreate them, to use for example when switching branches.

---
+++ scripts/reset_all_containers.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker compose down
+docker compose up -d
+docker compose exec web poetry run python manage.py migrate
+docker compose exec web poetry run python manage.py parameter_definitions
+docker compose exec web poetry run python manage.py populate --reset_all
