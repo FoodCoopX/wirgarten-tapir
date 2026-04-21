c8b6f065d23658cba6a7304b2df1b97c6f695b41
Théophile Madet <theo.madet@posteo.net>
2026-04-21 17:18:27 +0200

    fix: removed the wirgarten email template for admin emails #1133

--- tapir/wirgarten/service/email.py
+++ tapir/wirgarten/service/email.py
@@ -1,18 +1,13 @@
 from dataclasses import dataclass
-from datetime import datetime
 from typing import List
 
-from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 
 from tapir.configuration.parameter import get_parameter_value
 from tapir.log.models import EmailLogEntry
 from tapir.wirgarten.models import Member
 from tapir.wirgarten.parameter_keys import ParameterKeys
-from tapir.wirgarten.service.delivery import generate_future_deliveries
-from tapir.wirgarten.utils import format_date, get_today
 
 
 @dataclass
@@ -26,41 +21,13 @@ def send_email(
     to_email: List[str],
     subject: str,
     content: str,
-    variables: dict = None,
-    attachments: List[Attachment] = None,
-    cache: dict = None,
+    attachments: List[Attachment],
+    cache: dict,
 ):
-    """
-    Send an email to a list of recipients. The email is sent as HTML using the email/email_base.html template.
-
-    :param to_email: list of email addresses
-    :param subject: subject of the email
-    :param content: content of the email
-    :param variables: additional variables to be used in the email template
-    :param attachments: attached files
-    """
-
-    if variables is None:
-        variables = {}
-    if attachments is None:
-        attachments = []
-
-    variables.update(get_default_vars(to_email, cache=cache))
-    content = content.format(**variables)
-
-    email_body = render_to_string(
-        "wirgarten/email/email_base.html",
-        {
-            "content": content,
-            "subject": subject,
-            "preview_text": content,
-            "member_area_link": settings.SITE_URL,
-        },
-    )
 
     email = EmailMultiAlternatives(
         subject=subject,
-        body=email_body,
+        body=content,
         to=to_email,
         bcc=(
             [settings.EMAIL_AUTO_BCC]
@@ -76,56 +43,7 @@ def send_email(
     for attachment in attachments:
         email.attach(attachment.file_name, attachment.content, attachment.mime_type)
 
-    email.content_subtype = "html"
     email.send()
 
-    found_member = Member.objects.filter(email=to_email[0])
-    EmailLogEntry().populate(
-        email_message=email, user=found_member[0] if found_member.exists() else None
-    ).save()
-
-
-# all the vars stuff will be deprecated as soon as the mail module is going in production
-def get_default_vars(to_email, cache: dict):
-    variables = add_member_vars(to_email, cache=cache)
-    variables.update(add_general_vars(cache=cache))
-    return variables
-
-
-def add_general_vars(cache: dict):
-    today = get_today(cache=cache)
-    return {
-        "year_current": today.year,
-        "year_next": (today + relativedelta(years=1)).year,
-        "year_overnext": (today + relativedelta(years=2)).year,
-        "admin_name": get_parameter_value(ParameterKeys.SITE_ADMIN_NAME, cache=cache),
-        "site_name": get_parameter_value(ParameterKeys.SITE_NAME, cache=cache),
-        "admin_telephone": get_parameter_value(
-            ParameterKeys.SITE_ADMIN_TELEPHONE, cache=cache
-        ),
-        "admin_image": get_parameter_value(ParameterKeys.SITE_ADMIN_IMAGE, cache=cache),
-        "site_email": get_parameter_value(ParameterKeys.SITE_EMAIL, cache=cache),
-    }
-
-
-def add_member_vars(to_email, cache: dict):
-    try:
-        from tapir.wirgarten.models import Member
-
-        member = Member.objects.get(email=to_email[0])
-        future_deliveries = generate_future_deliveries(member, cache=cache)
-        return {
-            "member": member,
-            # FIXME: return None is not optimal...
-            "last_pickup_date": (
-                format_date(
-                    datetime.strptime(
-                        future_deliveries[-1]["delivery_date"], "%Y-%m-%d"
-                    ).date()
-                )
-                if len(future_deliveries) > 0
-                else None
-            ),
-        }
-    except Member.DoesNotExist:
-        return {}
+    member = Member.objects.filter(email=to_email[0]).first()
+    EmailLogEntry().populate(email_message=email, user=member).save()
