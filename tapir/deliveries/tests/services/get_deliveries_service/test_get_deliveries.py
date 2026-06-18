cdadffad9765d2bafa5e769a8422eab4a8d5bdeb
Théophile Madet <theo.madet@posteo.net>
2026-06-18 14:57:47 +0200

    test: improved tests for GetDeliveriesService.get_deliveries

--- tapir/deliveries/tests/services/get_deliveries_service/test_get_deliveries.py
+++ tapir/deliveries/tests/services/get_deliveries_service/test_get_deliveries.py
@@ -1,10 +1,16 @@
 import datetime
-from unittest.mock import patch, Mock
 
 from tapir.deliveries.services.get_deliveries_service import GetDeliveriesService
+from tapir.wirgarten.constants import WEEKLY, ODD_WEEKS
+from tapir.wirgarten.models import ProductType
+from tapir.wirgarten.parameter_keys import ParameterKeys
 from tapir.wirgarten.parameters import ParameterDefinitions
-from tapir.wirgarten.tests import factories
-from tapir.wirgarten.tests.factories import MemberFactory
+from tapir.wirgarten.tests.factories import (
+    MemberFactory,
+    ProductFactory,
+    SubscriptionFactory,
+    GrowingPeriodFactory,
+)
 from tapir.wirgarten.tests.test_utils import mock_timezone, TapirIntegrationTest
 
 
@@ -12,52 +18,75 @@ class TestGetDeliveriesServiceGetDeliveries(TapirIntegrationTest):
     @classmethod
     def setUpTestData(cls):
         ParameterDefinitions().import_definitions(bulk_create=True)
+        cls.product = ProductFactory.create(type__delivery_cycle=WEEKLY[0])
+        cls.growing_period = GrowingPeriodFactory.create(
+            start_date=datetime.date(year=2024, month=1, day=1)
+        )
+        GrowingPeriodFactory.create(start_date=datetime.date(year=2025, month=1, day=1))
 
     def setUp(self):
         super().setUp()
-        mock_timezone(self, factories.NOW)
+        mock_timezone(self, datetime.datetime(year=2023, month=3, day=15))
 
-    @patch.object(GetDeliveriesService, "build_delivery_object")
-    def test_getDeliveries_default_buildsDeliveryForAllDatesBetweenFromAndTo(
-        self, mock_build_delivery_object: Mock
-    ):
-        member = MemberFactory.create()
-        mock_build_delivery_object.return_value = Mock()
+        self.member = MemberFactory.create()
+        SubscriptionFactory.create(
+            member=self.member, product=self.product, period=self.growing_period
+        )
+
+    def test_getDeliveries_default_buildsDeliveryForAllDatesBetweenFromAndTo(self):
+
         cache = {}
-        deliveries = GetDeliveriesService.get_deliveries(
-            member=member,
+        actual_deliveries = GetDeliveriesService.get_deliveries(
+            member=self.member,
             date_from=datetime.date(year=2024, month=4, day=1),
             date_to=datetime.date(year=2024, month=4, day=30),
             cache=cache,
         )
 
-        self.assertEqual(4, len(deliveries))
-        self.assertEqual(4, mock_build_delivery_object.call_count)
-        delivery_dates_in_april_2024 = [
+        self.assertEqual(4, len(actual_deliveries))
+        expected_delivery_dates = [
             datetime.date(year=2024, month=4, day=day) for day in [3, 10, 17, 24]
         ]
-        for index, date in enumerate(delivery_dates_in_april_2024):
-            actual_call = mock_build_delivery_object.mock_calls[index]
-            _, args, kwargs = actual_call
-            self.assertEqual((), args)
+        for index, actual_delivery in enumerate(actual_deliveries):
             self.assertEqual(
-                {"member": member, "delivery_date": date, "cache": cache}, kwargs
+                expected_delivery_dates[index], actual_delivery["delivery_date"]
             )
 
-    @patch.object(GetDeliveriesService, "build_delivery_object")
     def test_getDeliveries_noDeliveryOnSomeDays_returnedListContainsOnlyDeliveredDays(
-        self, mock_build_delivery_object: Mock
+        self,
     ):
-        member = MemberFactory.create()
-        mock_build_delivery_object.side_effect = lambda member, delivery_date, cache: (
-            Mock() if delivery_date.day in [3, 17] else None
+        ProductType.objects.update(delivery_cycle=ODD_WEEKS[0])
+
+        cache = {}
+        actual_deliveries = GetDeliveriesService.get_deliveries(
+            member=self.member,
+            date_from=datetime.date(year=2024, month=4, day=1),
+            date_to=datetime.date(year=2024, month=4, day=30),
+            cache=cache,
+        )
+
+        self.assertEqual(2, len(actual_deliveries))
+        expected_delivery_dates = [
+            datetime.date(year=2024, month=4, day=day) for day in [10, 24]
+        ]
+        for index, actual_delivery in enumerate(actual_deliveries):
+            self.assertEqual(
+                expected_delivery_dates[index], actual_delivery["delivery_date"]
+            )
+
+    def test_getDeliveries_dateRangeIncludesNextGrowingPeriod_returnsDeliveriesForPlannedRenewedSubscriptions(
+        self,
+    ):
+        cache = {}
+        self._set_parameter(
+            key=ParameterKeys.SUBSCRIPTION_AUTOMATIC_RENEWAL, value=True
         )
 
         deliveries = GetDeliveriesService.get_deliveries(
-            member=member,
+            member=self.member,
             date_from=datetime.date(year=2024, month=4, day=1),
-            date_to=datetime.date(year=2024, month=4, day=30),
-            cache={},
+            date_to=datetime.date(year=2025, month=4, day=30),
+            cache=cache,
         )
 
-        self.assertEqual(2, len(deliveries))
+        self.assertEqual(57, len(deliveries))
