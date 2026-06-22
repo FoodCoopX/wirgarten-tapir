56ba667ca1dd3dff9e71923702a1771d8b0765b3
Théophile Madet <theo.madet@posteo.net>
2026-06-22 12:25:58 +0200

    fix: when creating a member via the modal from the member list, save the member properly

--- tapir/wirgarten/tests/unit/test_save_member_and_assign_number.py
+++ tapir/wirgarten/tests/unit/test_save_member_and_assign_number.py
@@ -1,8 +1,7 @@
 from unittest.mock import Mock, patch
 
-from tapir.wirgarten.tests.test_utils import TapirUnitTest
-
 from tapir.coop.services.member_number_service import MemberNumberService
+from tapir.wirgarten.tests.test_utils import TapirUnitTest
 from tapir.wirgarten.views.member.list.modals import save_member_and_assign_number
 
 
@@ -14,14 +13,17 @@ class TestSaveMemberAndAssignNumber(TapirUnitTest):
         return_value=True,
     )
     def test_saveMemberAndAssignNumber_eligible_savesMemberAndDelegatesToService(
-        self, mock_assign_if_eligible
+        self, mock_assign_if_eligible: Mock
     ):
+        form = Mock()
         member = Mock()
+        form.instance = member
 
-        save_member_and_assign_number(member)
+        save_member_and_assign_number(form)
 
-        member.save.assert_called_once_with()
+        form.save.assert_called_once_with()
         mock_assign_if_eligible.assert_called_once_with(member, cache={})
+        member.save.assert_not_called()
 
     @patch.object(
         MemberNumberService,
@@ -30,11 +32,14 @@ class TestSaveMemberAndAssignNumber(TapirUnitTest):
         return_value=False,
     )
     def test_saveMemberAndAssignNumber_notEligible_savesMemberTwice(
-        self, mock_assign_if_eligible
+        self, mock_assign_if_eligible: Mock
     ):
+        form = Mock()
         member = Mock()
+        form.instance = member
 
-        save_member_and_assign_number(member)
+        save_member_and_assign_number(form)
 
-        self.assertEqual(2, member.save.call_count)
+        form.save.assert_called_once_with()
+        member.save.assert_called_once_with()
         mock_assign_if_eligible.assert_called_once_with(member, cache={})
