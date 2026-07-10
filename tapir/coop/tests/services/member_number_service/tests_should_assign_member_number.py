ea0b2715bc1aaaf257dbb72df87bad3acf1c443b
Théophile Madet <theo.madet@posteo.net>
2026-07-10 13:57:15 +0200

    fix: infra#163, member numbers where given for users without any coop shares (that means they are not members are therefore should not get a member)

--- tapir/coop/tests/services/member_number_service/tests_should_assign_member_number.py
+++ tapir/coop/tests/services/member_number_service/tests_should_assign_member_number.py
@@ -1,13 +1,13 @@
 from unittest.mock import Mock, patch
 
-from tapir.wirgarten.tests.test_utils import TapirUnitTest
-
 from tapir.coop.services.member_number_service import MemberNumberService
 from tapir.coop.services.membership_cancellation_manager import (
     MembershipCancellationManager,
 )
 from tapir.utils.tests_utils import mock_parameter_value
+from tapir.wirgarten.models import CoopShareTransaction
 from tapir.wirgarten.parameter_keys import ParameterKeys
+from tapir.wirgarten.tests.test_utils import TapirUnitTest
 
 
 class TestShouldAssignMemberNumber(TapirUnitTest):
@@ -33,6 +33,7 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
             MemberNumberService.should_assign_member_number(member, cache=cache)
         )
 
+    @patch.object(CoopShareTransaction, "objects")
     @patch(
         "tapir.coop.services.member_number_service.legal_status_is_cooperative",
         autospec=True,
@@ -45,7 +46,10 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
         return_value=True,
     )
     def test_shouldAssignMemberNumber_onlyAfterTrialAndInCoopTrial_returnsFalse(
-        self, mock_is_in_coop_trial, mock_legal_status
+        self,
+        mock_is_in_coop_trial: Mock,
+        mock_legal_status: Mock,
+        mock_coop_share_transaction_objects: Mock,
     ):
         cache = {}
         mock_parameter_value(
@@ -55,13 +59,54 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
         )
         member = Mock()
         member.member_no = None
+        mock_coop_share_transaction_objects.filter.return_value.exists.return_value = (
+            True
+        )
 
         self.assertFalse(
             MemberNumberService.should_assign_member_number(member, cache=cache)
         )
         mock_is_in_coop_trial.assert_called_once_with(member)
         mock_legal_status.assert_called_once_with(cache=cache)
+        mock_coop_share_transaction_objects.filter.assert_called_once_with(
+            member=member
+        )
+        mock_coop_share_transaction_objects.filter.return_value.exists.assert_called_once_with()
 
+    @patch.object(CoopShareTransaction, "objects")
+    @patch(
+        "tapir.coop.services.member_number_service.legal_status_is_cooperative",
+        autospec=True,
+        return_value=True,
+    )
+    def test_shouldAssignMemberNumber_onlyAfterTrialAndHasNoCoopShares_returnsFalse(
+        self,
+        mock_legal_status: Mock,
+        mock_coop_share_transaction_objects: Mock,
+    ):
+        cache = {}
+        mock_parameter_value(
+            cache=cache,
+            key=ParameterKeys.MEMBER_NUMBER_ONLY_AFTER_TRIAL,
+            value=True,
+        )
+        member = Mock()
+        member.member_no = None
+        mock_coop_share_transaction_objects.filter.return_value.exists.return_value = (
+            False
+        )
+
+        self.assertFalse(
+            MemberNumberService.should_assign_member_number(member, cache=cache)
+        )
+
+        mock_legal_status.assert_called_once_with(cache=cache)
+        mock_coop_share_transaction_objects.filter.assert_called_once_with(
+            member=member
+        )
+        mock_coop_share_transaction_objects.filter.return_value.exists.assert_called_once_with()
+
+    @patch.object(CoopShareTransaction, "objects")
     @patch(
         "tapir.coop.services.member_number_service.legal_status_is_cooperative",
         autospec=True,
@@ -74,7 +119,10 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
         return_value=False,
     )
     def test_shouldAssignMemberNumber_onlyAfterTrialAndCoopTrialEnded_returnsTrue(
-        self, mock_is_in_coop_trial, mock_legal_status
+        self,
+        mock_is_in_coop_trial: Mock,
+        mock_legal_status: Mock,
+        mock_coop_share_transaction_objects: Mock,
     ):
         cache = {}
         mock_parameter_value(
@@ -84,12 +132,19 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
         )
         member = Mock()
         member.member_no = None
+        mock_coop_share_transaction_objects.filter.return_value.exists.return_value = (
+            True
+        )
 
         self.assertTrue(
             MemberNumberService.should_assign_member_number(member, cache=cache)
         )
         mock_is_in_coop_trial.assert_called_once_with(member)
         mock_legal_status.assert_called_once_with(cache=cache)
+        mock_coop_share_transaction_objects.filter.assert_called_once_with(
+            member=member
+        )
+        mock_coop_share_transaction_objects.filter.return_value.exists.assert_called_once_with()
 
     @patch(
         "tapir.coop.services.member_number_service.legal_status_is_cooperative",
@@ -103,7 +158,7 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
         return_value=True,
     )
     def test_shouldAssignMemberNumber_onlyAfterTrialAndInSubscriptionTrial_returnsFalse(
-        self, mock_is_in_subscription_trial, mock_legal_status
+        self, mock_is_in_subscription_trial: Mock, mock_legal_status: Mock
     ):
         cache = {}
         mock_parameter_value(
@@ -132,7 +187,7 @@ class TestShouldAssignMemberNumber(TapirUnitTest):
         return_value=False,
     )
     def test_shouldAssignMemberNumber_onlyAfterTrialAndSubscriptionTrialEnded_returnsTrue(
-        self, mock_is_in_subscription_trial, mock_legal_status
+        self, mock_is_in_subscription_trial: Mock, mock_legal_status: Mock
     ):
         cache = {}
         mock_parameter_value(
