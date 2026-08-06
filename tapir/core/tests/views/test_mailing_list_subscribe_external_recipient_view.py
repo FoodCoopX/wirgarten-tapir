1408e33d7d4510db5991c678577b480a39b3b88e
Théophile Madet <theo.madet@posteo.net>
2026-08-06 09:47:05 +0200

    test: #1122, added tests for MailingListSubscribeExternalRecipientView

---
+++ tapir/core/tests/views/test_mailing_list_subscribe_external_recipient_view.py
@@ -0,0 +1,126 @@
+from unittest.mock import Mock
+
+from django.test import override_settings
+from django.urls import reverse
+from rest_framework import status
+
+from tapir.core.services.mailman.mailing_list_provider import MailingListProvider
+from tapir.core.tests.mailman_test_helper import MailmanTestHelper, MockMailingListData
+from tapir.wirgarten.parameters import ParameterDefinitions
+from tapir.wirgarten.tests.factories import MemberFactory
+from tapir.wirgarten.tests.test_utils import TapirIntegrationTest
+
+
+class TestMailingListSubscribeExternalRecipientView(TapirIntegrationTest):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        ParameterDefinitions().import_definitions(bulk_create=True)
+
+    def test_post_loggedInAsNormalMember_returns403(self):
+        self.client.force_login(MemberFactory.create(is_superuser=False))
+
+        domain = MailmanTestHelper.mock_domain(
+            test=self,
+            mailing_list_datas=[
+                MockMailingListData(name="list_a"),
+                MockMailingListData(name="list_b"),
+            ],
+        )
+
+        post_data = {
+            "list_name": "list_b@example.com",
+            "address": "external@example.com",
+        }
+        response = self.client.post(
+            reverse("core:mailing_list_subscribe_external"), data=post_data
+        )
+
+        self.assertStatusCode(
+            response=response, expected_status_code=status.HTTP_403_FORBIDDEN
+        )
+        for mailing_list in domain.lists:
+            mailing_list.subscribe.assert_not_called()
+
+    @override_settings(MAILING_LISTS_ENABLED=False)
+    def test_post_mailingListsDisabled_returns403(self):
+        self.client.force_login(MemberFactory.create(is_superuser=True))
+
+        domain = MailmanTestHelper.mock_domain(
+            test=self,
+            mailing_list_datas=[
+                MockMailingListData(name="list_a"),
+                MockMailingListData(name="list_b"),
+            ],
+        )
+
+        post_data = {
+            "list_name": "list_b@example.com",
+            "address": "external@example.com",
+        }
+        response = self.client.post(
+            reverse("core:mailing_list_subscribe_external"), data=post_data
+        )
+
+        self.assertStatusCode(
+            response=response, expected_status_code=status.HTTP_403_FORBIDDEN
+        )
+        for mailing_list in domain.lists:
+            mailing_list.subscribe.assert_not_called()
+
+    def test_post_mailingListDoesntExist_returns404(self):
+        self.client.force_login(MemberFactory.create(is_superuser=True))
+
+        domain = MailmanTestHelper.mock_domain(
+            test=self,
+            mailing_list_datas=[
+                MockMailingListData(name="list_a"),
+                MockMailingListData(name="list_b"),
+            ],
+        )
+
+        post_data = {
+            "list_name": "list_c@example.com",
+            "address": "external@example.com",
+        }
+        response = self.client.post(
+            reverse("core:mailing_list_subscribe_external"), data=post_data
+        )
+
+        self.assertStatusCode(
+            response=response, expected_status_code=status.HTTP_404_NOT_FOUND
+        )
+        for mailing_list in domain.lists:
+            mailing_list.subscribe.assert_not_called()
+
+    def test_post_default_callsSubscribe(self):
+        self.client.force_login(MemberFactory.create(is_superuser=True))
+
+        MailmanTestHelper.mock_domain(
+            test=self,
+            mailing_list_datas=[
+                MockMailingListData(name="list_a"),
+                MockMailingListData(name="list_b"),
+            ],
+        )
+
+        post_data = {
+            "list_name": "list_b@example.com",
+            "address": "external@example.com",
+        }
+        response = self.client.post(
+            reverse("core:mailing_list_subscribe_external"), data=post_data
+        )
+
+        self.assertStatusCode(
+            response=response, expected_status_code=status.HTTP_200_OK
+        )
+        mailing_list_b: Mock = MailingListProvider.get_list_by_name_or_404(
+            list_name="list_b@example.com", cache={}
+        )
+        mailing_list_b.subscribe.assert_called_once_with(
+            address="external@example.com", invitation=True
+        )
+        mailing_list_a: Mock = MailingListProvider.get_list_by_name_or_404(
+            list_name="list_a@example.com", cache={}
+        )
+        mailing_list_a.subscribe.assert_not_called()
